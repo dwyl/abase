431e786368b876e1eeb8639989611792230c2d05
Elias <elias0789@gmail.com>
2016-09-26 16:12:03 +0100

    change module inputs to inject table_name directly instead of entire schema object

--- test/sql_gen.test.js
+++ test/sql_gen.test.js
@@ -44,7 +44,7 @@ tape('::init should generate SQL to create a table if none exists', function (t)
 });
 
 tape('::insert should generate SQL to insert a column into a table', function (t) {
-  var query = sqlGen.insert(schema, { email: 'me@poop.com' });
+  var query = sqlGen.insert(schema.table_name, { email: 'me@poop.com' });
 
   t.equal(query[0], 'INSERT INTO "user_data" (email) VALUES ($1)', 'Generate parameterised query');
   t.deepEqual(query[1], ['me@poop.com'], 'Generate values for parameterised query');
@@ -52,7 +52,7 @@ tape('::insert should generate SQL to insert a column into a table', function (t
 });
 
 tape('::update should generate SQL to update a column in a table', function (t) {
-  var query = sqlGen.update(schema, { email: 'me@poop.com' });
+  var query = sqlGen.update(schema.table_name, { email: 'me@poop.com' });
 
   t.equal(query[0], 'UPDATE "user_data" SET email=$1', 'Generate parameterised query');
   t.deepEqual(query[1], ['me@poop.com'], 'Generate values for parameterised query');
