d4d9d3dee58f59d3143e7c1e7a17459e92ad85e0
SimonLab <simon.labondance@gmail.com>
2016-09-26 23:44:06 +0100

    add tests for loading user schema with abase

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,15 @@
-var tape = require('tape');
-var Hapi = require('hapi');
-var path = require('path');
-var fs = require('fs');
-
-fs.writeFileSync(path.join(__dirname, 'schema.json'), JSON.stringify({}), 'utf8');
-
 var abase = require('../lib/index.js');
+var fs = require('fs');
+var Hapi = require('hapi');
+var tape = require('tape');
+var path = require('path');
 
 tape('check wrong path throws error', function (t) {
   var server = new Hapi.Server();
   server.connection({ port: 0 });
 
   var opts = {
-    wrong_path: ''
+    wrong: path.join(__dirname, 'fixtures', 'schema.json')
   };
 
   server.register({ register: abase, options: opts }, function (err) {
@@ -23,44 +20,59 @@ tape('check wrong path throws error', function (t) {
   });
 });
 
-tape('check no error is thrown if we have the user_schema_path key', function (t) {
+tape('Attempt to load a wrong content config schema', function (t) {
   var server = new Hapi.Server();
-  server.connection({ port: 0});
+  server.connection({ port: 0 });
 
   var opts = {
-    user_schema_path: path.join(__dirname, 'schema.json')
+    user_schema_path: path.join(__dirname, 'fixtures', 'wrong_schema.json')
   };
 
-  var schema = require('./schema.json');
-
-  t.deepEqual(schema, {}, 'schema is empty');
-
   server.register({ register: abase, options: opts }, function (err) {
-    t.ok(!err, 'No error thrown');
-
-    var schema = JSON.parse(fs.readFileSync(path.join(__dirname, 'schema.json')));
-
-    t.ok(schema.email, 'Schema has email property');
+    t.ok(err, 'Error thrown');
     server.stop(function () {
       t.end();
     });
   });
 });
 
-tape('check schema is read and no keys are updated', function (t) {
+tape('Load abase plugin properly', function (t) {
   var server = new Hapi.Server();
-  server.connection({ port: 0});
+  server.connection({ port: 0 });
 
   var opts = {
-    user_schema_path: path.join(__dirname, 'schema.json')
+    user_schema_path: path.join(__dirname, 'fixtures', 'right_schema.json')
   };
 
   server.register({ register: abase, options: opts }, function (err) {
-    t.ok(!err, 'No error thrown');
-
+    t.ok(!err, 'Error thrown');
     server.stop(function () {
       t.end();
     });
   });
 });
 
+tape('Load properly abase plugin and add default values', function (t) {
+  var server = new Hapi.Server();
+  server.connection({ port: 0 });
+
+  var opts = {
+    user_schema_path: path.join(__dirname, 'fixtures', 'schema.json')
+  };
+  var schemaBefore = JSON.parse(fs.readFileSync(opts.user_schema_path, 'utf8'));
+  t.deepEqual(schemaBefore, {}, 'The schema is empty before abase loading');
+  server.register({ register: abase, options: opts }, function (err) {
+    t.ok(!err, 'No error thrown');
+    var schemaAfter = JSON.parse(fs.readFileSync(opts.user_schema_path, 'utf8'));
+    var expected = {
+      email: {type: 'email'},
+      password: {type: 'password'}
+    };
+    t.deepEqual(schemaAfter, expected, 'The schema contains the default values after abase loaded');
+    server.stop(function () {
+      //reset the schema for next tests
+      fs.writeFileSync(opts.user_schema_path, JSON.stringify({}), 'utf8');
+      t.end();
+    });
+  });
+});
