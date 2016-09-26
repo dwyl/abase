3110924d56e92ea924433602b5df69ab645b9380
Elias <elias0789@gmail.com>
2016-09-26 15:24:24 +0100

    rename previous 'update' method to 'insert' (since that's what it does). Write and test an update method; issue #11

--- test/sql_gen.test.js
+++ test/sql_gen.test.js
@@ -45,10 +45,18 @@ tape('::init should generate SQL to create a table if none exists', function (t)
 
 // tape('::update should generate empty string on invalid input', function () {});
 
+tape('::insert should generate SQL to insert a column into a table', function (t) {
+  var query = sqlGen.insert(schema, {email: 'me@poop.com'});
+
+  t.equal(query[0], 'INSERT INTO "user_data" (email) VALUES ($1)', 'Generate parameterised query');
+  t.deepEqual(query[1], ['me@poop.com'], 'Generate values for parameterised query');
+  t.end();
+});
+
 tape('::update should generate SQL to update a column in a table', function (t) {
   var query = sqlGen.update(schema, {email: 'me@poop.com'});
 
-  t.equal(query[0], 'INSERT INTO "user_data" (email) VALUES ($1)', 'Generate parameterised query');
+  t.equal(query[0], 'UPDATE "user_data" SET email=$1', 'Generate parameterised query');
   t.deepEqual(query[1], ['me@poop.com'], 'Generate values for parameterised query');
   t.end();
 });
