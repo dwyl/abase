866aae6afa0ef5e1f333b3f95fc4f084c80021fb
Elias <elias0789@gmail.com>
2016-09-27 12:36:40 +0100

    add utils module to help with DRYing out the sql_gen module

---
+++ test/utils.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var test = require('tape');
+var _ = require('../lib/utils.js');
+
+test('::values w/ default keys value', function (t) {
+  var o = {a: 1, b: 2};
+  var result = _.values(o);
+
+  t.equal(result[0], o.a, 'Key "a" matches');
+  t.equal(result[1], o.b, 'Key "b" matches');
+  t.end();
+});
