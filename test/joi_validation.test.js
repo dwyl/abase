f7f4251784a8eb45a5212c6b99ca6bb58a99b10a
Jack Rans <jwrans@gmail.com>
2016-09-28 19:26:12 +0100

    :shirt: fix linting errors

--- test/joi_validation.test.js
+++ test/joi_validation.test.js
@@ -1,19 +1,23 @@
+'use strict';
+
 var test = require('tape');
-var Joi  = require('joi');
+var Joi = require('joi');
 
 var schemaCreator = require('../lib/joi_validation.js');
-var exampleSchema = require('./example_schema.js')
+var exampleSchema = require('./example_schema.js');
 
 var joiAssert = function (object, schema) {
-  return function () { Joi.assert(object, schema) };
-}
+  return function () {
+    Joi.assert(object, schema);
+  };
+};
 
 test('schemaCreator returns simplejoi which behaves as expected', function (t) {
   var schema = schemaCreator(exampleSchema, ['dob']);
 
-  t.ok( schema.isJoi, 'when configured correctly we get a joi object back');
+  t.ok(schema.isJoi, 'when configured correctly we get a joi object back');
   t.doesNotThrow(
-    joiAssert({dob: '12-21-2012'}, schema),
+    joiAssert({ dob: '12-21-2012' }, schema),
     'valid email doesn\'t throw, true handled as no arguments for condition'
   );
   t.doesNotThrow(
@@ -21,7 +25,10 @@ test('schemaCreator returns simplejoi which behaves as expected', function (t) {
     'dob not specifically required yet'
   );
   t.throws(
-    joiAssert({dob: '12-21-2012', username: 'ttt'}, schema),
+    joiAssert({
+      dob: '12-21-2012',
+      username: 'ttt'
+    }, schema),
     'username given when not wanted'
   );
 
@@ -30,8 +37,14 @@ test('schemaCreator returns simplejoi which behaves as expected', function (t) {
 
 test('schemaCreator with custom options', function (t) {
   var schema = schemaCreator(exampleSchema, [
-    { name: 'email', options: { required: true } },
-    { name: 'dob', options: { valid: ['12-21-2012', '12-22-2012'] } },
+    {
+      name: 'email',
+      options: { required: true }
+    },
+    {
+      name: 'dob',
+      options: { valid: ['12-21-2012', '12-22-2012'] }
+    },
     'username'
   ]);
 
@@ -44,12 +57,18 @@ test('schemaCreator with custom options', function (t) {
     'min info given, another example of argument as true for email key'
   );
   t.throws(
-    joiAssert({ email: 't@g.com', dob: '12-25-2012' }, schema),
+    joiAssert({
+      email: 't@g.com',
+      dob: '12-25-2012'
+    }, schema),
     'specified dobs not given, example of user of array of arguments'
     + ' and additional requirements'
   );
   t.throws(
-    joiAssert({ email: 't@g.com', username: 'ts'  }, schema),
+    joiAssert({
+      email: 't@g.com',
+      username: 'ts'
+    }, schema),
     'username too short, example of single argument given'
     + ' existing requirements'
   );
@@ -59,7 +78,7 @@ test('schemaCreator with custom options', function (t) {
 
 test('schemaCreator invalid fields', function (t) {
   t.throws(
-    function () { schemaCreator(exampleSchema, ['email', 'not_configured']); },
+    function () { schemaCreator(exampleSchema, ['email', 'not_configured']) },
     'if a field passed which is not in config object we throw for now'
   );
 
