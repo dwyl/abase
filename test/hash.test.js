ec5775e9e4abafd14edbe193fe4f61927a1724cf
Jack Rans <jwrans@gmail.com>
2016-09-29 10:22:41 +0100

    :heavy_plus_sign: Function to hash data according to config  + Function to take some data and hash asynchronously according to field settings in config.  + Fields can be given password or hash prop set as true to be told to hash.  + New object given back, old not mutated.
    Related: 52

---
+++ test/hash.test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var test = require('tape');
+var compareSync = require('bcrypt').compareSync;
+
+var hash = require('../lib/hash.js');
+
+var testConfig = { fields: {
+  secretAnswer: { hash: true },
+  password: { password: true },
+  email: {}
+} };
+
+test('no need to hash, cb instantaneous w/o err but new object', function (t) {
+  var testData = { email: 't@g.com' };
+
+  hash(testConfig, testData, function (err, res) {
+    t.equal(err, null, 'no chance for error creation');
+    t.deepEqual(res, { email: 't@g.com' }, 'object shallowly equal');
+    t.notEqual(res, testData, 'new object always returned');
+
+    t.end();
+  });
+});
+
+test('needs to be hashed', function (t) {
+  var testData = {
+    email: 'no@need.com',
+    password: 'hash me',
+    secretAnswer: 'please'
+  };
+
+  hash(testConfig, testData, function (err, res) {
+    t.notOk(err, 'no error should be produced');
+    t.deepEqual(
+      testData,
+      {
+        email: 'no@need.com',
+        password: 'hash me',
+        secretAnswer: 'please'
+      },
+      'original object not mutated'
+    );
+    t.equal(res.email, testData.email, 'email remains the same');
+    t.notEqual(res, testData, 'new object always returned');
+    t.ok(
+      compareSync(testData.password, res.password),
+      'field tagged password hashed ok'
+    );
+    t.ok(
+      compareSync(testData.secretAnswer, res.secretAnswer),
+      'field tagged hash, hashed ok'
+    );
+
+    t.end();
+  });
+});
+
+test('forcing a bcrypt error with invalid args (won\'t happen)', function (t) {
+  var testData = {
+    password: 'validString',
+    secretAnswer: 42
+  };
+
+  hash(testConfig, testData, function (err) {
+    t.ok(err, 'error created');
+    t.deepEqual(
+      testData,
+      {
+        password: 'validString',
+        secretAnswer: 42
+      },
+      'original object not mutated'
+    );
+
+    t.end();
+  });
+});
