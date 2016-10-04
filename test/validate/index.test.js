482670bf1ddd284f8365668bba3a744521a148ab
Elias <elias0789@gmail.com>
2016-10-04 12:55:58 +0100

    fix linter errors

--- test/validate/index.test.js
+++ test/validate/index.test.js
@@ -22,13 +22,19 @@ test('Register validation plugin w/ schemaPath option', function (t) {
 
     t.equal(
       server.abaseValidate(['email']).toString(),
-      Joi.object().keys({ email: Joi.string().email() }).toString(),
+      Joi.object()
+        .keys({ email: Joi.string().email() })
+        .toString(),
       'Generated joi object matches for email field'
     );
 
     t.equal(
       server.abaseValidate(['username']).toString(),
-      Joi.object().keys({ username: Joi.string().min(3).max(20) }).toString(),
+      Joi.object()
+        .keys({ username: Joi.string()
+            .min(3)
+            .max(20) })
+        .toString(),
       'Generated joi object matches for username field'
     );
 
@@ -36,9 +42,12 @@ test('Register validation plugin w/ schemaPath option', function (t) {
       server.abaseValidate(['email', 'username', 'dob']).toString(),
       Joi.object().keys({
         email: Joi.string().email(),
-        username: Joi.string().min(3).max(20),
+        username: Joi.string()
+          .min(3)
+          .max(20),
         dob: Joi.date()
-      }).toString(),
+      })
+      .toString(),
       'Generated joi object matches for multiple fields'
     );
 
